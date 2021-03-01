a560e4fabfad9d7b1208aa9157608b2300ba7022
denliedesign <customdenlie@gmail.com>
2021-03-01 01:53:46 -0600

    summer reg btn

--- resources/views/summer.blade.php
+++ resources/views/summer.blade.php
@@ -5,6 +5,9 @@
 
     <div class="container">
         <h1 class="head-white py-4" style="text-decoration: underline;">Summer</h1>
+        <div class="d-flex justify-content-center">
+            <a href="https://app.thestudiodirector.com/encoredancetheatreofs/portal.sd?page=Login" target="_blank" style="text-decoration: none;"><button class="btn btn-lg btn-danger my-4" style="margin: 0 auto; display: block;">REGISTER NOW!</button></a>
+        </div>
         <div class="row my-3">
             <div class="col-sm mb-3">
                 <img src="/images/summer-yard-sale.jpg" alt="dance camp promotion" class="img-fluid rounded shadow">
@@ -17,9 +20,12 @@
             <div class="col-sm mb-3">
                 <img src="/images/mermaid-tumbling.png" alt="mermaid tumbling" class="img-fluid rounded shadow">
             </div>
-            <div class="col-sm mb-3">
+            <div class="col-sm">
                 <img src="/images/summer-ballet-intensive-2021.jpg" alt="summer ballet intensive" class="img-fluid rounded shadow">
             </div>
+        </div>
+        <div class="d-flex justify-content-center">
+            <a href="https://app.thestudiodirector.com/encoredancetheatreofs/portal.sd?page=Login" target="_blank" style="text-decoration: none;"><button class="btn btn-lg btn-danger my-4" style="margin: 0 auto; display: block;">REGISTER NOW!</button></a>
         </div>
 {{--        <div class="row">--}}
 {{--            <div class="col-sm">--}}
