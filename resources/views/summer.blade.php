3d54906fd569593f14db3af96d639ae05edc7eea
denliedesign <customdenlie@gmail.com>
2021-03-18 12:45:53 -0500

    more summer

--- resources/views/summer.blade.php
+++ resources/views/summer.blade.php
@@ -5,60 +5,66 @@
 
     <div class="container">
         <h1 class="head-white py-4" style="text-decoration: underline;">Summer</h1>
-        <div class="d-flex justify-content-center">
-            <a href="https://app.thestudiodirector.com/encoredancetheatreofs/portal.sd?page=Login" target="_blank" style="text-decoration: none;"><button class="btn btn-lg btn-danger my-4" style="margin: 0 auto; display: block;">REGISTER NOW!</button></a>
-        </div>
+        <h2 class="text-center">Summer Camps & Intensives</h2>
         <div class="row my-3">
             <div class="col-sm mb-3">
                 <img src="/images/summer-yard-sale.jpg" alt="dance camp promotion" class="img-fluid rounded shadow">
+                <div class="d-flex justify-content-center">
+                    <a href="https://app.thestudiodirector.com/encoredancetheatreofs/portal.sd?page=Login" target="_blank" style="text-decoration: none;"><button class="btn btn-lg btn-danger my-4" style="margin: 0 auto; display: block;">REGISTER NOW!</button></a>
+                </div>
             </div>
             <div class="col-sm mb-3">
                 <img src="/images/summer-yard-sale-2.jpg" alt="more dance camp promotions" class="img-fluid rounded shadow">
+                <div class="d-flex justify-content-center">
+                    <a href="https://app.thestudiodirector.com/encoredancetheatreofs/portal.sd?page=Login" target="_blank" style="text-decoration: none;"><button class="btn btn-lg btn-danger my-4" style="margin: 0 auto; display: block;">REGISTER NOW!</button></a>
+                </div>
             </div>
         </div>
         <div class="row my-3">
             <div class="col-sm mb-3">
-                <img src="/images/mermaid-tumbling.png" alt="mermaid tumbling" class="img-fluid rounded shadow">
+                <img src="/images/mermaid-tumbling.PNG" alt="mermaid tumbling" class="img-fluid rounded shadow">
+                <div class="d-flex justify-content-center">
+                    <a href="https://app.thestudiodirector.com/encoredancetheatreofs/portal.sd?page=Login" target="_blank" style="text-decoration: none;"><button class="btn btn-lg btn-danger my-4" style="margin: 0 auto; display: block;">REGISTER NOW!</button></a>
+                </div>
             </div>
             <div class="col-sm">
                 <img src="/images/summer-ballet-intensive-2021.jpg" alt="summer ballet intensive" class="img-fluid rounded shadow">
+                <div class="d-flex justify-content-center">
+                    <a href="https://app.thestudiodirector.com/encoredancetheatreofs/portal.sd?page=Login" target="_blank" style="text-decoration: none;"><button class="btn btn-lg btn-danger my-4" style="margin: 0 auto; display: block;">REGISTER NOW!</button></a>
+                </div>
             </div>
         </div>
-        <div class="d-flex justify-content-center">
+
+        <h2 class="text-center">Summer Classes</h2>
+        <div class="row my-3">
+            <div class="col-sm mb-3">
+                <img src="/images/summer-classes-1.PNG" alt="summer classes" class="img-fluid rounded shadow">
+                <div class="d-flex justify-content-center">
             <a href="https://app.thestudiodirector.com/encoredancetheatreofs/portal.sd?page=Login" target="_blank" style="text-decoration: none;"><button class="btn btn-lg btn-danger my-4" style="margin: 0 auto; display: block;">REGISTER NOW!</button></a>
         </div>
-{{--        <div class="row">--}}
-{{--            <div class="col-sm">--}}
-{{--                <img src="images/summer-schedule.jpg" alt="" class="img-fluid">--}}
-{{--            </div>--}}
-{{--        </div>--}}
-
-{{--        <a href="https://app.thestudiodirector.com/encoredancetheatreofs/portal.sd?page=Login" target="_blank" style="text-decoration: none;"><button class="btn btn-lg btn-danger my-4" style="margin: 0 auto; display: block;">REGISTER NOW!</button></a>--}}
-
-{{--        <h1 class="head-white py-4" style="text-decoration: underline;">Summer Intensives</h1>--}}
-{{--        <div class="row">--}}
-{{--            <div class="col-sm">--}}
-{{--                <img src="images/summer-ballet-intensive.jpg" alt="" class="img-fluid">--}}
-{{--            </div>--}}
-{{--            <div class="col-sm">--}}
-{{--                <img src="images/staystrong.jpg" alt="" class="img-fluid">--}}
-{{--            </div>--}}
-{{--        </div>--}}
-
-{{--        <a href="https://app.thestudiodirector.com/encoredancetheatreofs/portal.sd?page=Login" target="_blank" style="text-decoration: none;"><button class="btn btn-lg btn-danger my-4" style="margin: 0 auto; display: block;">REGISTER NOW!</button></a>--}}
-
-{{--        <h1 class="head-white py-4" style="text-decoration: underline;">Summer Camps</h1>--}}
-{{--        <div class="row">--}}
-{{--            <div class="col-sm">--}}
-{{--                <img src="images/minisummercamps.jpg" alt="" class="img-fluid">--}}
-{{--            </div>--}}
-{{--            <div class="col-sm">--}}
-{{--                <img src="images/minisummercamps2.jpg" alt="" class="img-fluid">--}}
-{{--            </div>--}}
-{{--        </div>--}}
-
-{{--        <a href="https://app.thestudiodirector.com/encoredancetheatreofs/portal.sd?page=Login" target="_blank" style="text-decoration: none;"><button class="btn btn-lg btn-danger my-4" style="margin: 0 auto; display: block;">REGISTER NOW!</button></a>--}}
-</div>
+            </div>
+            <div class="col-sm mb-3">
+                <img src="/images/summer-classes-2.PNG" alt="summer classes" class="img-fluid rounded shadow">
+                <div class="d-flex justify-content-center">
+            <a href="https://app.thestudiodirector.com/encoredancetheatreofs/portal.sd?page=Login" target="_blank" style="text-decoration: none;"><button class="btn btn-lg btn-danger my-4" style="margin: 0 auto; display: block;">REGISTER NOW!</button></a>
+        </div>
+            </div>
+        </div>
+        <div class="row my-3">
+            <div class="col-sm mb-3">
+                <img src="/images/summer-classes-3.PNG" alt="summer classes" class="img-fluid rounded shadow">
+                <div class="d-flex justify-content-center">
+            <a href="https://app.thestudiodirector.com/encoredancetheatreofs/portal.sd?page=Login" target="_blank" style="text-decoration: none;"><button class="btn btn-lg btn-danger my-4" style="margin: 0 auto; display: block;">REGISTER NOW!</button></a>
+        </div>
+            </div>
+            <div class="col-sm mb-3">
+                <img src="/images/summer-classes-4.PNG" alt="summer classes" class="img-fluid rounded shadow">
+                <div class="d-flex justify-content-center">
+            <a href="https://app.thestudiodirector.com/encoredancetheatreofs/portal.sd?page=Login" target="_blank" style="text-decoration: none;"><button class="btn btn-lg btn-danger my-4" style="margin: 0 auto; display: block;">REGISTER NOW!</button></a>
+        </div>
+            </div>
+        </div>
+    </div>
 
 
 
