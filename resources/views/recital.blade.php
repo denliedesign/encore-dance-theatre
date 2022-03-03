186aae0f22c28c3b47f7d94adbe00c227d2a8175
denliedesign <customdenlie@gmail.com>
2022-03-02 23:48:29 -0600

    recital fixes

--- resources/views/recital.blade.php
+++ resources/views/recital.blade.php
@@ -10,14 +10,18 @@
                 <div>
                     <img src="/images/mobile-app-2022.jpg" alt="mobile app info" class="img-fluid shadow rounded">
                     <div class="d-flex justify-content-center">
-                        <a href="https://play.google.com/store/apps/details?id=com.encoredancetheatre.mi" target="_blank"><img src="/images/edt-apple-store.png" alt="apple mobile app" class="m-3 img-fluid"></a>
-                        <a href="https://apps.apple.com/app/id1528239603" target="_blank"><img src="/images/edt-google-play.png" alt="google mobile app" class="m-3 img-fluid"></a>
+                        <a href="https://play.google.com/store/apps/details?id=com.encoredancetheatre.mi" target="_blank"><img src="/images/edt-google-play.png" alt="apple mobile app" class="m-3 img-fluid"></a>
+                        <a href="https://apps.apple.com/app/id1528239603" target="_blank"><img src="/images/edt-apple-store.png" alt="google mobile app" class="m-3 img-fluid"></a>
                     </div>
                 </div>
             </div>
             <div class="col-sm">
                 <p style="font-size: 1.5em;">
-                    The opportunity to perform in a recital is an exciting, empowering and confidence-building part of the training process. This is where we love to watch our students learn about the theatre, rehearsals and teamwork while they enjoy the opportunity to showcase their skills in a professionally run production. <span style="background: #0092BC;" class="text-white font-weight-bold rounded p-1 m-1">Recitals are a wonderful way for students to showcase the results of their hard work and for family and friends to join in the pride of their achievement.</span> We love giving all students the chance to perform for their family and friends in a supportive environment and encourage even our youngest and newest performers to be involved.
+                    The opportunity to perform in a recital is an exciting, empowering and confidence-building part of the training process. This is where we love to watch our students learn about the theatre, rehearsals and teamwork while they enjoy the opportunity to showcase their skills in a professionally run production.
+{{--                    <span style="background: #0092BC;" class="text-white font-weight-bold rounded p-1 m-1">--}}
+                        Recitals are a wonderful way for students to showcase the results of their hard work and for family and friends to join in the pride of their achievement.
+{{--                    </span>--}}
+                    We love giving all students the chance to perform for their family and friends in a supportive environment and encourage even our youngest and newest performers to be involved.
                     <br><br>
                     This is without a doubt the highlight of our year and we are giddy with excitement to show you how hard our dancers have been working!
                 </p>
@@ -41,14 +45,16 @@
                     <ion-icon name="today-outline"></ion-icon>
                 </div>
                 <b><u>Days / Times:</u></b>
-                <br>
-                Arrival Time Show #1: 10:30am
-                <br>
-                Arrival Time Show #2: 3:30pm
+{{--                <br>--}}
+{{--                Arrival Time Show #1: 10:30am--}}
+{{--                <br>--}}
+{{--                Arrival Time Show #2: 3:30pm--}}
                 <br>
                 Show #1: Saturday, June 4th @ 11am
                 <br>
                 Show #2: Saturday, June 4th @ 4pm
+                <br>
+                *Dancers arrive 30 minutes before show time
             </div>
         </div>
     </div>
