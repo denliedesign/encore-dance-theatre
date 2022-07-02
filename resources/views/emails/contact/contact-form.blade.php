3341cd55058baffefd3ecd87ad8836d94fa3012f
denliedesign <customdenlie@gmail.com>
2022-07-02 12:36:12 -0500

    trial mail fix

--- resources/views/emails/contact/contact-form.blade.php
+++ resources/views/emails/contact/contact-form.blade.php
@@ -2,3 +2,6 @@
 <br><strong>Email</strong> {{ $data['email'] }}
 <br><strong>Message</strong>
 <br>{{ $data['message'] }}
+<br><br>
+<strong>**DO NOT REPLY**</strong>
+<br>Please compose a new message
--- resources/views/emails/trial/trial-form.blade.php
+++ resources/views/emails/trial/trial-form.blade.php
@@ -3,3 +3,6 @@
 <br><strong>Phone Number</strong> {{ $trial['phone'] }}
 <br><strong>Student Name</strong> {{ $trial['studentName'] }}
 <br><strong>Birthdate</strong> {{ $trial['birthdate'] }}
+<br><br>
+<strong>**DO NOT REPLY**</strong>
+<br>Please compose a new message
