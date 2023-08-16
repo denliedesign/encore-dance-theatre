6f8cafe526a39577c3f5359819cc16f5ee7e4300
denliedesign <customdenlie@gmail.com>
2023-08-15 20:36:48 -0500

    alana staff

--- resources/views/staff.blade.php
+++ resources/views/staff.blade.php
@@ -392,7 +392,7 @@ Bio coming soon
                                     <div class="card-header faculty-card-header" id="headingOne">
                                         <h2 class="mb-0 d-flex justify-content-center">
                                             <button class="btn btn-link" style="height: 84px;" type="button" data-toggle="collapse" data-target="#collapseTerry" aria-expanded="true" aria-controls="collapseOne">
-                                                Stacy Harding Terry
+                                                Stacy Terry
                                             </button>
                                         </h2>
                                     </div>
@@ -835,6 +835,38 @@ Bio coming soon
 {{--                        </div>--}}
 {{--                    </div>--}}
 {{--                </div>--}}
+                <div class="col-sm master-faculty-col">
+                    <div class="row py-4">
+                        <div class="col-sm-4 d-flex justify-content-sm-end justify-content-center">
+                            <div class="headshot">
+                                <img src="/images/staff-alana.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="col-sm-8">
+                            <div class="accordion faculty-bio" id="accordionExample">
+                                <div class="card">
+                                    <div class="card-header faculty-card-header" id="headingOne">
+                                        <h2 class="mb-0 d-flex justify-content-center">
+                                            <button class="btn btn-link" style="height: 84px;" type="button" data-toggle="collapse" data-target="#collapseAlana" aria-expanded="true" aria-controls="collapseOne">
+                                                Alana Scheuerer
+                                            </button>
+                                        </h2>
+                                    </div>
+
+                                    <div id="collapseAlana" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                                        <div class="card-body faculty-bio-txt">
+                                            Alana holds an M.A. in American Dance Studies from Florida State University. Growing up as an award winning competitive dancer in South Florida, she specializes in tap. Alana helped found and run FSU’s first tap dance team, Seminole Tap Troupe, competed with student-run competitive dance team Seminole Dance Force, and served as hospitality coordinator for Dance Marathon. Alana has experience with hosting cultural stages at art festivals, producing and voicing NBC Comcast commercials, hosting live radio, and teaching dance workshops. She holds a seat on Coral Springs Public Art Committee and currently serves as the 85th Anniversary Miss Broward County, with her community service initiative of TAP: Turn up Artistic Participation. She works full time operating special events at the Museum of Discovery Science in downtown Fort Lauderdale, having worked with brands such as LEGO, Publix, and Joe DiMaggio Children’s Hospital. In March 2023, she led plans and operations for the 8,000 attendee Visit Lauderdale Science Festival. She’s excited to join the EDT Family and share her love for tap!
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
                 <div class="col-sm master-faculty-col">
                     <div class="row py-4">
                         <div class="col-sm-4 d-flex justify-content-sm-end justify-content-center">
