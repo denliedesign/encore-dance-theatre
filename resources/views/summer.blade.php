c3a19b47eddde562e4e24b561e093d30acb46703
denliedesign <customdenlie@gmail.com>
2024-04-03 10:30:40 -0500

    dance classes gfx update

--- resources/views/summer.blade.php
+++ resources/views/summer.blade.php
@@ -12,7 +12,7 @@
         <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2 my-3">
             <img src="/images/2024-dance-classes-1.png" alt="summer classes" class="img-fluid p-2">
             <img src="/images/2024-dance-classes-2.png" alt="summer classes" class="img-fluid p-2">
-            <img src="/images/2024-dance-classes-3.png" alt="summer classes" class="img-fluid p-2">
+            <img src="/images/2024-dance-classes-3b.png" alt="summer classes" class="img-fluid p-2">
             <img src="/images/2024-dance-classes-4.png" alt="summer classes" class="img-fluid p-2">
         </div>
         <div class="d-flex justify-content-center">
