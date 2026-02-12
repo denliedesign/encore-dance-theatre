f3e5aa110b22fd4b46e964ac1ce61e810cfd1efc
denliedesign <customdenlie@gmail.com>
2026-02-12 11:21:31 -0600

    summer

--- resources/views/summer.blade.php
+++ resources/views/summer.blade.php
@@ -5,29 +5,32 @@
 
     <div class="container">
         <h1 class="head-white py-4" style="text-decoration: underline;">Summer</h1>
-        <h2 class="text-center">Summer Classes</h2>
+{{--        <h2 class="text-center">Summer Classes</h2>--}}
         <div class="d-flex justify-content-center">
             <a href="https://app.thestudiodirector.com/encoredancetheatreofs/portal.sd?page=Login" target="_blank" style="text-decoration: none;"><button class="btn btn-lg btn-danger my-4" style="margin: 0 auto; display: block;">REGISTER NOW!</button></a>
         </div>
-        <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2 my-3">
-            <img src="/images/2025-summer-1.png" alt="summer classes" class="img-fluid p-2">
-            <img src="/images/2025-summer-2.png" alt="summer classes" class="img-fluid p-2">
-            <img src="/images/2025-summer-3.png" alt="summer classes" class="img-fluid p-2">
-            <img src="/images/2025-summer-4.png" alt="summer classes" class="img-fluid p-2">
-            <img src="/images/2025-summer-5.png" alt="summer classes" class="img-fluid p-2">
+        <div class="d-flex justify-content-center">
+            <img src="/images/summer-26.png" class="img-fluid" alt="">
         </div>
+{{--        <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2 my-3">--}}
+{{--            <img src="/images/2025-summer-1.png" alt="summer classes" class="img-fluid p-2">--}}
+{{--            <img src="/images/2025-summer-2.png" alt="summer classes" class="img-fluid p-2">--}}
+{{--            <img src="/images/2025-summer-3.png" alt="summer classes" class="img-fluid p-2">--}}
+{{--            <img src="/images/2025-summer-4.png" alt="summer classes" class="img-fluid p-2">--}}
+{{--            <img src="/images/2025-summer-5.png" alt="summer classes" class="img-fluid p-2">--}}
+{{--        </div>--}}
         <div class="d-flex justify-content-center">
             <a href="https://app.thestudiodirector.com/encoredancetheatreofs/portal.sd?page=Login" target="_blank" style="text-decoration: none;"><button class="btn btn-lg btn-danger my-4" style="margin: 0 auto; display: block;">REGISTER NOW!</button></a>
         </div>
-        <h2 class="text-center">Summer Camps</h2>
-        <div class="d-flex justify-content-center">
-            <a href="https://app.thestudiodirector.com/encoredancetheatreofs/portal.sd?page=Login" target="_blank" style="text-decoration: none;"><button class="btn btn-lg btn-danger my-4" style="margin: 0 auto; display: block;">REGISTER NOW!</button></a>
-        </div>
-        <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2 my-3">
-            <img src="/images/2025-summer-camps.png" alt="summer classes" class="img-fluid p-2">
-        </div>
-        <div class="d-flex justify-content-center">
-            <a href="https://app.thestudiodirector.com/encoredancetheatreofs/portal.sd?page=Login" target="_blank" style="text-decoration: none;"><button class="btn btn-lg btn-danger my-4" style="margin: 0 auto; display: block;">REGISTER NOW!</button></a>
-        </div>
+{{--        <h2 class="text-center">Summer Camps</h2>--}}
+{{--        <div class="d-flex justify-content-center">--}}
+{{--            <a href="https://app.thestudiodirector.com/encoredancetheatreofs/portal.sd?page=Login" target="_blank" style="text-decoration: none;"><button class="btn btn-lg btn-danger my-4" style="margin: 0 auto; display: block;">REGISTER NOW!</button></a>--}}
+{{--        </div>--}}
+{{--        <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2 my-3">--}}
+{{--            <img src="/images/2025-summer-camps.png" alt="summer classes" class="img-fluid p-2">--}}
+{{--        </div>--}}
+{{--        <div class="d-flex justify-content-center">--}}
+{{--            <a href="https://app.thestudiodirector.com/encoredancetheatreofs/portal.sd?page=Login" target="_blank" style="text-decoration: none;"><button class="btn btn-lg btn-danger my-4" style="margin: 0 auto; display: block;">REGISTER NOW!</button></a>--}}
+{{--        </div>--}}
     </div>
 @endsection
